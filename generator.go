3e0e1fcff9a11a1b88511e237e75ac93a2c75740
Jan Fooken <dev@janvhs.com>
2023-12-22 23:38:51 +0100

    Fix  #7: Do not generate unnecessary modules
    This patch adds the Adw-1 namespace to the package definition.
    Now, the generator only outputs a module fot the libadwaita package.
    Before, it generated all modules, it could find GIRs for, which leads to failing verification.
    
    Reference: https://github.com/diamondburned/gotk4-adwaita/issues/7

--- generator.go
+++ generator.go
@@ -15,8 +15,13 @@ const (
 var Data = genmain.Overlay(
 	gendata.Main,
 	genmain.Data{
-		Module:       adwaitaModule,
-		Packages:     []genmain.Package{{Name: "libadwaita-1"}},
+		Module: adwaitaModule,
+		Packages: []genmain.Package{
+			{
+				Name:       "libadwaita-1",
+				Namespaces: []string{"Adw-1"},
+			},
+		},
 		PkgGenerated: []string{"adw"},
 		PkgExceptions: []string{
 			"go.mod",
